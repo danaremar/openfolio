404a6403e3021e2f6e3b01d37be48fa326e71400
danaremar <56022332+danaremar@users.noreply.github.com>
2023-11-23 18:42:26 +0100

    GitHub pages upload

---
+++ dist/assets/config.ts
@@ -0,0 +1,508 @@
+export const features = {
+    home: true,
+    qualification: true,
+    experience: true,
+    skills: true,
+    products: true,
+    services: true,
+    contact: true
+}
+
+export const profile = {
+    name: "Daniel Arellano",
+    role: [
+        "Software Engineer",
+        "Project Manager",
+        "Cybersecurity Engineer"
+    ],
+    shakeHand: true,
+    fastContacts: true,
+    description: "Dedicated Software Engineer with a focus on efficient project management. Demonstrates strong performance in delivering high-quality solutions. Combines technical expertise with excellent interpersonal skills, fostering a collaborative and social work environment.",
+}
+
+export const qualifications = [
+    {
+        title: "IT Management Master's Degree",
+        starts: "October 2022",
+        ends: "July 2023",
+        location: "University of Seville",
+        text: ""
+    },
+    {
+        title: "Cybersecurity Master's Degree",
+        starts: "October 2021",
+        ends: "September 2022",
+        location: "University of Seville",
+        text: ""
+    },
+    {
+        title: "Software Engineering Degree",
+        starts: "September 2017",
+        ends: "September 2021",
+        location: "University of Seville",
+        text: ""
+    },
+    {
+        title: "English B2",
+        starts: "September 2019",
+        location: "Language Institute (University of Seville)",
+        text: ""
+    }
+]
+
+export const experience = [
+    {
+        title: "Cybersecurity consultant",
+        company: "Plexus Security",
+        starts: "August 2023",
+        ends: "Actual",
+        knowledge: [
+            "Led team",
+            "Scrum methodology & Jira",
+            "Automations with Microsoft PowerAutomate & Python",
+            "Cybersecurity rating: BitSight & Security Scorecard",
+            "Early Warning & Qualys",
+        ]
+    },
+    {
+        title: "Cybersecurity consultant",
+        company: "Tarlogic",
+        starts: "April 2022",
+        ends: "July 2023",
+        knowledge: [
+            "Automations with Microsoft PowerAutomate & Python",
+            "Creating dashboards with PowerBI & Apache Superset",
+            "Early Warning & Qualys",
+            "Scrum methodology & Jira",
+            "Develop new initiatives",
+        ]
+    },
+    {
+        title: "Product Software developer",
+        company: "Tier1",
+        starts: "July 2020",
+        ends: "September 2021",
+        knowledge: [
+            "Design new feature & support",
+            "Java, Spring, Bootstrap, HTML, jQuery, JavaFX, Java tags, OpenAPI/Swagger",
+            "Teaching",
+        ]
+    }
+]
+
+export const skills = [
+    {
+        group: "Software Engineer",
+        stack: [
+            {
+                name: "Java + Spring Boot + Gradle/Maven",
+                percentage: 90,
+                link: "https://github.com/danaremar/agix/tree/develop/webflux"
+            },
+            {
+                name: "Angular + Bootstrap + HTML + Typescript",
+                percentage: 90,
+                link: "https://github.com/danaremar/openfolio"
+            },
+            {
+                name: "Python + pandas + requests + FastAPI",
+                percentage: 70
+            },
+            {
+                name: "Git + GitHub + CI/CD",
+                percentage: 70,
+                link: "https://github.com/danaremar"
+            },
+            {
+                name: "Docker",
+                percentage: 60,
+                link: "https://github.com/danaremar/agix/tree/develop"
+            },
+            {
+                name: "Kubernetes",
+                percentage: 40
+            },
+            {
+                name: "SQL (MariaDB, MySQL, Oracle, MS Server)",
+                percentage: 70,
+                link: "https://github.com/danaremar/agix/tree/develop/webflux"
+            },
+            {
+                name: "MongoDB",
+                percentage: 70,
+                link: "https://github.com/danaremar/agix/tree/develop/webflux"
+            },
+        ]
+    },
+    {
+        group: "Management",
+        stack: [
+            {
+                name: "Scrum",
+                percentage: 95
+            },
+            {
+                name: "Kanban",
+                percentage: 75
+            },
+            {
+                name: "PMBOK",
+                percentage: 50
+            },
+            {
+                name: "Jira",
+                percentage: 70
+            },
+            {
+                name: "Figma + Balsamiq (UX/UI)",
+                percentage: 85,
+                link: "https://www.figma.com/file/5hlsFypsYtljGoFKG80Nwp/manapi?type=design&mode=design&t=3E0gS25WF7fLN5XS-1"
+            },
+            {
+                name: "Microsoft Word",
+                percentage: 90,
+                link: "assets/products/manapi/manapi-doc.pdf"
+            },
+            {
+                name: "Microsoft Powerpoint",
+                percentage: 90,
+                link: "assets/products/manapi/manapi-slides.pdf"
+            },
+            {
+                name: "Pricing analysis",
+                percentage: 90,
+                link: "assets/products/manapi/manapi-slides.pdf"
+            },
+        ]
+    },
+    {
+        group: "Cybersecurity",
+        stack: [
+            {
+                name: "Cryptography + hash + certificates",
+                percentage: 90
+            },
+            {
+                name: "Metasploit + airodump",
+                percentage: 50
+            },
+            {
+                name: "Qualys + OpenVAS + nmap + Wireshark",
+                percentage: 80
+            },
+            {
+                name: "Vulnerability Management",
+                percentage: 100
+            },
+            {
+                name: "Virtual Machines + Containers",
+                percentage: 80
+            },
+            {
+                name: "Security Scorecard",
+                percentage: 90
+            },
+            {
+                name: "ISO 27000 + NIST + OWASP",
+                percentage: 30
+            },
+        ]
+    }
+]
+
+export const softskills = [
+    {
+        name: "Teamwork",
+        icon: "bi bi-people"
+    },
+    {
+        name: "Leadership",
+        icon: "bi bi-person-bounding-box"
+    },
+    {
+        name: "Problem solving",
+        icon: "bi bi-bug"
+    },
+    {
+        name: "Creative thinking",
+        icon: "bi bi-brush"
+    }
+]
+
+export const contacts = [
+    {
+        platform: "Email",
+        info: "danaremar@alum.us.es",
+        icon: "bi bi-envelope",
+        link: "danaremar@alum.us.es"
+    },
+    {
+        platform: "GitHub",
+        info: "@danaremar",
+        icon: "bi bi-github",
+        link: "https://github.com/danaremar"
+    },
+    {
+        platform: "LinkedIn",
+        info: "danielarellano99",
+        icon: "bi bi-linkedin",
+        link: "https://www.linkedin.com/in/danielarellano99/"
+    },
+    {
+        platform: "Telegram",
+        info: "daniarellano99",
+        icon: "bi bi-telegram",
+        link: "https://t.me/daniarellano99"
+    }
+]
+
+export const products = [
+    {
+        name: "AGIX",
+        description: [
+            "All-in-one project management solution, designed to streamline your workflow and boost productivity. With features like sprint management, tasks, kanban, gantt, time tracker and estimations.",
+            "The system is built on a microservices architecture, incorporating an API Gateway, and operates as a single-page application. Various plans are available, each offering distinct features, rates, and quotas. Security is implemented using the JWT standard with claims tokens.",
+        ],
+        secondary: "Angular + Bootstrap + Spring Boot (Java + Gradle) + MongoDB + MariaDB",
+        images: [
+            "products/agix/03.jpg",
+            "products/agix/04.jpg",
+            "products/agix/05.jpg",
+            "products/agix/06.jpg",
+            "products/agix/01.jpg",
+            "products/agix/02.jpg",
+            "products/agix/agix-kanban.png",
+        ],
+        sources: [
+            {
+                icon: "bi bi-github",
+                link: "https://github.com/danaremar/iMan"
+            },
+            {
+                icon: "bi bi-easel",
+                link: "./assets/products/manapi/manapi-slides.pdf"
+            },
+            {
+                icon: "bi bi-file-earmark-text",
+                link: "./assets/products/manapi/manapi-doc.pdf"
+            },
+        ]
+    },
+    {
+        name: "Openfolio",
+        description: [
+            "Open-source personal portfolio using Angular and Bootstrap, easily configurable with just one file.",
+            "The portfolio features both dark and light themes, boasting a responsive and minimalist design.",
+            "It has been constructed as an open-source project, providing the opportunity for individuals to create their own instances as well. Check it out on GitHub!"
+        ],
+        secondary: "Angular + Bootstrap",
+        images: [
+            "products/portfolio/01.jpg",
+            "products/portfolio/02.jpg",
+            "products/portfolio/03.jpg",
+            "products/portfolio/04.jpg",
+            "products/portfolio/05.jpg",
+            "products/portfolio/06.jpg",
+            "products/portfolio/07.jpg",
+            "products/portfolio/08.jpg",
+            "products/portfolio/09.jpg",
+            "products/portfolio/10.jpg",
+        ],
+        sources: [
+            {
+                icon: "bi bi-window-fullscreen",
+                link: "https://danaremar.github.io/openfolio/"
+            },
+            {
+                icon: "bi bi-github",
+                link: "https://github.com/danaremar/openfolio"
+            }
+        ]
+    },
+    {
+        name: "Taxi trip",
+        description: [
+            "Conducting AI analysis on New York taxi data involves examining distances, prices, and trip durations.",
+            "The primary objective is to apply various clustering algorithms to discern patterns and identify clusters based on trip duration and distance."
+        ],
+        secondary: "R (Caret + dbscan + ggplot2)",
+        images: [
+            "products/taxi/clusters.png",
+            "products/taxi/clusters-2NN.png",
+            "products/taxi/clusters-dbscan.png",
+        ],
+        sources: [
+            {
+                icon: "bi bi-github",
+                link: "https://github.com/danaremar/taxi-trip-fido"
+            },
+            {
+                icon: "bi bi-easel",
+                link: "./assets/products/taxi/FID-G5-taxi.pdf"
+            },
+            {
+                icon: "bi bi-file-earmark-text",
+                link: "./assets/products/taxi/taxi_trip_fid.html"
+            },
+        ]
+    },
+    {
+        name: "Calc game",
+        description: [
+            "Mental response improvement game has been developed to enhance cognitive abilities through simple mathematical calculations",
+            "Designed using Android Studio specifically for Android devices, featuring both light and dark themes"
+        ],
+        secondary: "Android Studio + Gradle + Material",
+        images: [
+            "products/calc-game/calc_game_04.jpg",
+            "products/calc-game/calc_game_01.jpg",
+            "products/calc-game/calc_game_05.jpg",
+            "products/calc-game/calc_game_02.jpg",
+            "products/calc-game/calc_game_03.jpg",
+        ],
+        sources: [
+            {
+                icon: "bi bi-joystick",
+                link: "./assets/products/calc-game/calc-game.apk"
+            }
+        ]
+    },
+]
+
+export const services = [
+    {
+        name: "Portfolio",
+        description: "Introducing a personalized portfolio, personal website, or CV that showcases the best of you. From personal details to skills and services, all presented in a top-notch, responsive design. Make your online presence impeccable and attractive—because you deserve to stand out!",
+        tags: [
+            "Static web",
+            "Openfolio"
+        ],
+        images: [
+            "products/portfolio/01.jpg",
+            "products/portfolio/02.jpg",
+        ],
+        pricing: [
+            {
+                name: "Basic",
+                description: "2 services + 4 products",
+                price: "75€"
+            },
+            {
+                name: "Advanced",
+                description: "5 services + 10 products",
+                price: "149€"
+            },
+            {
+                name: "Deployment",
+                description: "5 small changes/month + domain + deployment",
+                price: "15€/month"
+            },
+        ]
+    },
+    {
+        name: "Website",
+        description: "Craft a striking online presence with our static website design. Ideal for businesses, stores, providers, and personal use. Embrace a minimalist, responsive, and visually appealing layout that captivates visitors. Elevate your online identity with elegance and functionality!",
+        tags: [
+            "Static web",
+            "Angular",
+            "Bootstrap/Tailwind",
+            "HTML"
+        ],
+        images: [
+            "products/ui-ux/figma-login.jpg",
+        ],
+        pricing: [
+            {
+                name: "Basic",
+                description: "1 page + responsive",
+                price: "149€"
+            },
+            {
+                name: "Advanced",
+                description: "3 pages + responsive + image optimization + lighthouse optimization + SEO",
+                price: "299€"
+            },
+            {
+                name: "Basic deployment",
+                description: "5 small changes/month + domain + deployment",
+                price: "15€/month"
+            },
+            {
+                name: "Advanced deployment",
+                description: "15 small changes/month + domain + deployment",
+                price: "30€/month"
+            },
+        ]
+    },
+    {
+        name: "API",
+        description: "Top-notch REST API crafted by a cybersecurity expert. Armed with OpenAPI/Swagger documentation, JWT security, MVC pattern, and features like filtering and pagination, it's a powerhouse. Plus, streamline deployment with Docker, ensuring a secure and efficient process. This API isn't just functional; it's a security-savvy masterpiece, ready to deploy with ease.",
+        tags: [
+            "Spring Boot",
+            "MongoDB",
+            "MariaDB / MySQL",
+            "OpenAPI / Swagger",
+            "JWT",
+            "Security patterns"
+        ],
+        images: [
+            "products/manapi/manapi-swagger.jpg"
+        ],
+        pricing: [
+            {
+                name: "Basic",
+                description: "2 models + 5 endpoint + OpenAPI + JWT + Validations",
+                price: "199€"
+            },
+            {
+                name: "Advanced",
+                description: "5 models + 10 endpoints + basic features + rates/quotas + feature toggling + SSO",
+                price: "399€"
+            },
+            {
+                name: "Deployment",
+                description: "Deployment in AWS / Google / Azure",
+                price: "From 15€/month"
+            }
+        ]
+    },
+    {
+        name: "Design UI/UX",
+        description: "Craft top-notch UI/UX designs with quality using Figma or Balsamiq. Elevate user experiences through thoughtful design, whether it's the seamless flow of Figma or the rapid prototyping of Balsamiq. Your interface will not only look stunning but also prioritize user satisfaction. Let your design speak excellence and functionality.",
+        tags: [
+            "UI",
+            "Figma",
+            "UX",
+            "Balsamiq"
+        ],
+        images: [
+            "products/ui-ux/figma-kanban.jpg",
+            "products/ui-ux/figma-task.jpg",
+            "products/ui-ux/balsamiq-login.jpg",
+            "products/ui-ux/balsamiq-incidents.jpg",
+            "products/ui-ux/balsamiq-gantt.jpg"
+        ],
+        pricing: [
+            {
+                name: "Basic Balsamiq (UX)",
+                description: "10 mockups",
+                price: "50€"
+            },
+            {
+                name: "Advanced Balsamiq (UX)",
+                description: "25 mockups",
+                price: "125€"
+            },
+            {
+                name: "Basic Figma (UI)",
+                description: "3 pages",
+                price: "50€"
+            },
+            {
+                name: "Advanced Figma (UI)",
+                description: "10 pages",
+                price: "125€"
+            },
+        ]
+    }
+]
